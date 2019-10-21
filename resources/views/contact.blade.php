33846bc84758cef139a70ee60c4cfa7fa698e792
AnNV <an.nguyen@co-well.vn>
2019-10-21 13:56:59 +0700

    feat : blade template in lavarel

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,45 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel</title>
+@extends('layouts.app')
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('title', 'Posts page')
 
-    <style>
-        html, body {
-            height: 100%;
-        }
+@section('sidebar')
+    @parent
+    <script>alert('Im AN')</script>
+@endsection
 
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 96px;
-        }
-    </style>
-</head>
-<body>
-<div class="container">
-    <div class="content">
-        <h1>Hello Contact</h1>
-    </div>
-</div>
-</body>
-</html>
+@section('content')
+    <h2>This is my content of posts page</h2>
+@endsection
